cfdb5dc95814eb3c8396fb81c2b763c63fc37fb2
designaky <designaky@gmail.com>
2021-05-05 14:30:57 +0100

    adding the routes to server

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const server = express();
 const logger = require("./src/middleware/logger.js");
-
 const cookieParser = require("cookie-parser");
-
 const staticHandler = express.static("public");
-server.use(staticHandler);
-
 const bodyParser = express.urlencoded();
 
+// Routes
+const signup = require("./src/handlers/signup");
+
+server.use(staticHandler);
 server.use(logger.logger);
 
 server.get("/", (request, response) => {
   response.send(`<h1>Hello</h1>`);
 });
 
-server.post("/submit", bodyParser, (request, response) => {});
+server.get("/signup", (request, response) => {
+  response.send(`<h1>Hello</h1>`);
+});
+
+// Sign up route
+server.post("/signup", bodyParser, signup.get);
+server.post("/submit", bodyParser, signup.post);
+
+
 
 server.use((req, res) => {
   res.status(404).send(`<h1>Not found</h1>`);
