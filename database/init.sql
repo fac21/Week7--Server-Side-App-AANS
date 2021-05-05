fa200c83967fecf95a821d7af6e841c215c549c5
Amy Kuner <amykuner1@gmail.com>
2021-05-05 12:27:32 +0100

    initialising init.sql schema

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,49 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users, games, stats, sessions CASCADE;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    hash_password TEXT NOT NULL
 );
+
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY, 
+    game_name TEXT NOT NULL
+);
+
+
+CREATE TABLE stats (
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    game_id INTEGER REFERENCES games(id) ON DELETE CASCADE,
+    score INTEGER,
+    last_played timestamp
+);
+
+CREATE TABLE sessions (
+    sid TEXT PRIMARY KEY,
+   data JSON NOT NULL
+);
+
+INSERT INTO users (username, email, hash_password) VALUES (
+    'amyk', 
+    'amyk@amyk.com', 
+    'drglshiusb\gis\nbiuABEiu29bjksJ'
+);
+
+INSERT INTO sessions (sid, data) VALUES 
+(
+    'sessionsjgbKG', 
+    '{"info":"things"}'
+);
+
+INSERT INTO games (game_name) VALUES (
+    'hangman'
+    );
+
+INSERT INTO stats ( user_id, game_id, score, last_played) VALUES
+  (1,  1, 2, (SELECT CURRENT_TIMESTAMP));
 
 COMMIT;
