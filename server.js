279047bda95c5eaed401c119f47840feffeeed3b
ByteSized <neville.keemer@gmail.com>
2021-05-06 17:13:17 +0100

    Added error handler middleware and /error route.
    Co-authored-by: Amy Kuner <amykuner1@gmail.com>

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const server = express();
 const session = require("express-session");
 const auth = require("./src/auth");
 
-// Middleware 
+// Middleware
 const cookieParser = require("cookie-parser");
 const logger = require("./src/middleware/logger.js");
+const errorHandler = require("./src/middleware/errorHandler.js");
 const staticHandler = express.static("public");
 const flash = require("connect-flash");
 const bodyParser = express.urlencoded();
@@ -14,9 +15,6 @@ const bodyParser = express.urlencoded();
 const home = require("./src/handlers/home");
 const signup = require("./src/handlers/signup");
 const logIn = require("./src/handlers/logIn");
-
-
-
 
 server.use(
   session({
@@ -30,7 +28,6 @@ server.use(flash());
 server.use(staticHandler);
 server.use(logger.logger);
 server.use(cookieParser(process.env.COOKIE_SECRET));
-
 
 server.get("/", home.getLayout);
 
@@ -46,9 +43,17 @@ server.post("/log-in", bodyParser, logIn.post);
 server.get("/sign-up", signup.get);
 server.post("/sign-up", bodyParser, signup.post);
 
-server.use((request, response) => {
-  response.status(404).send(`<h1>Not found</h1>`);
+server.get("/error", (req, res, next) => {
+  const fakeError = new Error("uh oh");
+  fakeError.status = 403;
+  next(fakeError);
 });
+
+server.use(errorHandler.handleErrors);
+
+// server.use((request, response) => {
+//   response.status(404).send(`<h1>Not found</h1>`);
+// });
 
 const PORT = process.env.PORT || 3000;
 
